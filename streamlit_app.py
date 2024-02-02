8dfbad2e74df8a2719e235523991835268304faa
Tom <thomas.chaumard@ecov.fr>
2024-02-02 11:15:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,40 +1,20 @@
-import altair as alt
-import numpy as np
-import pandas as pd
 import streamlit as st
 
-"""
-# Welcome to Streamlit!
+st.title("Monte Carlo Simulations :spades:")
+st.write(
+    """ 
+    A demo showing how Monte Carlo simulations can be used to predict the future stock price for P&G.
 
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:.
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
+    A Monte Carlo simulation is a mathematical technique, which is used to estimate the possible outcomes of an uncertain event. 
+    A Monte Carlo analysis consists of input variables, output variables, and a mathematical model. 
 
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
+    This demo is using the following mathematical model:
 
-num_points = st.slider("Number of points in spiral yoman", 1, 10000, 1100)
-num_turns = st.slider("Number of turns in spiral", 1, 300, 31)
+             Stock Price Today = Stock Price Yesterday * e^r
 
-indices = np.linspace(0, 1, num_points)
-theta = 2 * np.pi * num_turns * indices
-radius = indices
+    To calculate r the geometric Brownian motion (GBM) model is used.
 
-x = radius * np.cos(theta)
-y = radius * np.sin(theta)
+    Start by connecting to your Snowflake account, using the **Snowflake connect** link in the sidebar.
 
-df = pd.DataFrame({
-    "x": x,
-    "y": y,
-    "idx": indices,
-    "rand": np.random.randn(num_points),
-})
-
-st.altair_chart(alt.Chart(df, height=700, width=700)
-    .mark_point(filled=True)
-    .encode(
-        x=alt.X("x", axis=None),
-        y=alt.Y("y", axis=None),
-        color=alt.Color("idx", legend=None, scale=alt.Scale()),
-        size=alt.Size("rand", legend=None, scale=alt.Scale(range=[1, 150])),
-    ))
+    """
+)
