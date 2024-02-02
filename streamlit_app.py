e12c97b6e45b002bbf6516e4abcfd2eaa41efa7a
Tom <thomas.chaumard@ecov.fr>
2024-02-02 11:19:45 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,20 +1,130 @@
+import calendar
+import datetime
+from datetime import datetime, timedelta
+from pathlib import Path
+from time import sleep
+
+import numpy as np
+import pandas as pd
+import plotly_express as px
 import streamlit as st
+from sklearn.ensemble import RandomForestRegressor
 
-st.title("Monte Carlo Simulations :spades:")
-st.write(
-    """ 
-    A demo showing how Monte Carlo simulations can be used to predict the future stock price for P&G.
 
-    A Monte Carlo simulation is a mathematical technique, which is used to estimate the possible outcomes of an uncertain event. 
-    A Monte Carlo analysis consists of input variables, output variables, and a mathematical model. 
+# %%
+@st.cache
+def load_data():
+    bikes_data_url = 'https://gist.githubusercontent.com/AurelienMassiot/b3070dab9e31dd119242648b4d27c9b4/raw/e35965ebe409d31fcfe59d5574e33641a2b43728/bike_sharing_demand_train.csv'
+    data = pd.read_csv(bikes_data_url)
+    return data
 
-    This demo is using the following mathematical model:
 
-             Stock Price Today = Stock Price Yesterday * e^r
+def preprocess_data(dataframe):
+    dataframe['date'] = dataframe['datetime'].apply(lambda x: x.split()[0])
+    dataframe['hour'] = dataframe['datetime'].apply(lambda x: x.split()[1].split(':')[0])
+    dataframe['weekday'] = dataframe['date'].apply(
+        lambda date_string: calendar.day_name[datetime.strptime(date_string, '%Y-%m-%d').weekday()])
+    dataframe['month'] = dataframe['date'].apply(
+        lambda date_string: calendar.month_name[datetime.strptime(date_string, '%Y-%m-%d').month])
+    dataframe['season'] = dataframe['season'].map({1: 'Spring', 2: 'Summer', 3: 'Fall', 4: 'Winter'})
+    return dataframe
 
-    To calculate r the geometric Brownian motion (GBM) model is used.
 
-    Start by connecting to your Snowflake account, using the **Snowflake connect** link in the sidebar.
+st.title('Bike Sharing demand')
+df = load_data()
+df_preprocessed = preprocess_data(df.copy())
+st.write(df_preprocessed)
 
-    """
+st.title('Data exploration')
+# %% barplots
+st.subheader('Barplots')
+mean_counts_by_hour = pd.DataFrame(df_preprocessed.groupby(['hour', 'season'], sort=True)['count'].mean()).reset_index()
+fig1 = px.bar(mean_counts_by_hour, x='hour', y='count', color='season', height=400)
+barplot_chart = st.write(fig1)
+
+# %% timeseries
+st.subheader('Timeseries')
+df_preprocessed['datetime'] = pd.to_datetime(df_preprocessed['datetime'])
+fig2 = px.line(df_preprocessed, x='datetime', y='temp')
+ts_chart = st.plotly_chart(fig2)
+
+# %% boxplots
+st.subheader('Boxplots')
+categories_count = ['casual', 'registered', 'count']
+chosen_count = st.sidebar.selectbox(
+    'Which counts for boxplots?',
+    categories_count
 )
+
+fig3 = px.box(df_preprocessed, x='weekday', y=chosen_count, color='season', notched=True)
+boxplot_chart = st.plotly_chart(fig3)
+
+st.title('Modelization')
+
+# %% Modelization
+X = df_preprocessed[['temp', 'humidity']]
+y = df_preprocessed['count']
+model_rf = RandomForestRegressor(max_depth=2, n_estimators=10)
+model_rf.fit(X, y)
+
+
+# %% Online timeseries
+
+def generate_new_row(df):
+    time_end_new_data = df['datetime'].max() + timedelta(hours=1)
+    random_number_temp = np.random.uniform(df['temp'].min(), df['temp'].max(),
+                                           size=(1), )
+    random_number_humidity = np.random.uniform(df['humidity'].min(), df['humidity'].max(),
+                                               size=(1), )
+    new_df = pd.DataFrame({'datetime': [time_end_new_data],
+                           'temp': random_number_temp,
+                           'humidity': random_number_humidity,
+                           'predicted': [True]})
+    return new_df
+
+
+def add_row(df, new_row_df):
+    return pd.concat([df, new_row_df], axis=0).reset_index(drop=True)
+
+
+def generate_new_prediction(df, row, model):
+    time_end_new_data = df['datetime'].max() + timedelta(hours=1)
+    X_pred = row[['temp', 'humidity']]
+    y_pred = model.predict(X_pred)
+    new_df = pd.DataFrame({'datetime': [time_end_new_data],
+                           'count': y_pred,
+                           'predicted': [True]})
+    return new_df
+
+
+def animate(df, column, chart):
+    fig = px.line(df, x='datetime', y=column, color='predicted')
+    chart.plotly_chart(fig)
+
+
+n_rows_to_display = 50
+df_for_predictions = df_preprocessed.copy()
+df_for_predictions['predicted'] = False
+fig = px.line(df_for_predictions.tail(n_rows_to_display), x='datetime', y='count', color='predicted')
+online_ts_chart = st.plotly_chart(fig)
+new_row_info = st.empty()
+predicted_row_warning = st.empty()
+
+if st.sidebar.checkbox('Stream and predict on new data'):
+    bar = st.progress(0)
+    for i in range(11):
+        # get new row
+        new_row = generate_new_row(df_for_predictions)
+        new_row_info.info(f'Received new values: \n'
+                          f'temperature={np.round(new_row["temp"].values[0], 2)} - \n'
+                          f'humidity={np.round(new_row["humidity"].values[0], 2)} \n')
+        # predict
+        new_prediction = generate_new_prediction(df_for_predictions, new_row, model_rf)
+        predicted_row_warning.warning(f'Predicted count: {np.round(new_prediction["count"].values[0], 2)}')
+        # concatenate predicted row
+        df_for_predictions = add_row(df_for_predictions, new_prediction)
+        # animate
+        animate(df_for_predictions.tail(n_rows_to_display), 'count', online_ts_chart)
+        bar.progress(i * 10)
+        # wait
+        sleep(0.1)
